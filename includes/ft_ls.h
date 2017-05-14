6e119dadd581a443703dc858dab04152490678e5
Marco Nunnari <marnunna@gmail.com>
2017-05-15 01:39:40 +0200

    good start

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -13,7 +13,19 @@
 #ifndef FT_LS_H
 # define FT_LS_H
 
+# include <stdio.h>
+
 # include "libft.h"
 # include <dirent.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <time.h>
+
+# ifndef __APPLE__
+#  define st_atimespec            st_atim
+#  define st_mtimespec            st_mtim
+#  define st_ctimespec            st_ctim
+#  define st_birthtimespec        st_birthtim
+# endif
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -12,7 +12,54 @@
 
 #include "ft_ls.h"
 
+int		ft_btree_strcmp(t_btree *b1, t_btree *b2)
+{
+	return (ft_strcmp((char*)b1->content, (char*)b2->content));
+}
+
+char		*get_type(mode_t mode)
+{
+	switch (mode & S_IFMT)
+	{
+	case S_IFBLK:  return ("block device");
+	case S_IFCHR:  return ("character device");
+	case S_IFDIR:  return ("directory");
+	case S_IFIFO:  return ("FIFO/pipe");
+	case S_IFLNK:  return ("symlink");
+	case S_IFREG:  return ("regular file");
+	case S_IFSOCK: return ("socket");
+	default:       return ("unknown?");
+	}
+}
+
+void		ft_btree_putstr(t_btree *b)
+{
+	struct stat info;
+
+	printf("%s: \n", (char*)b->content);
+	stat((char*)b->content, &info);
+	printf("\t%s: %s\n", "Type", get_type(info.st_mode));
+	printf("\t%s: %lo\n", "Modes", (unsigned long)info.st_mode);
+	printf("\t%s: %ld\n", "Links number", (long)info.st_nlink);
+	printf("\t%s: %ld\n", "Owner", (long)info.st_uid);
+	printf("\t%s: %ld\n", "Owner group", (long)info.st_gid);
+	printf("\t%s: %lld\n", "Size", (long long)info.st_size);
+	printf("\t%s: %s\n", "Last file modification", ctime(&info.st_mtime));
+}
+
 int		main()
 {
-	ft_putstr("heila");
+	t_btree *entries = NULL;
+	DIR *dirp = opendir(".");
+	struct dirent *entry;
+	while ((entry = readdir(dirp)))
+	{
+		if (entries == NULL)
+			entries = ft_btreenew(entry->d_name, 0);
+		else
+			ft_btreeadd(&entries, ft_btreenew(entry->d_name, 0),
+				&ft_btree_strcmp);
+	}
+	ft_btree_apply_infix(entries, &ft_btree_putstr);
+	closedir(dirp);
 }
